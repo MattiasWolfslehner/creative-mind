796f6550f68df896f162e7e165001e7405157d3d
Jonas <121523551+jonasfroeller@users.noreply.github.com>
2023-10-24 07:29:34 +0000

    #17

---
+++ frontend/public/js/store.js
@@ -0,0 +1,9 @@
+export default class localStorageAction {
+	static async save(name, data) {
+		localStorage.setItem(name, JSON.stringify(data))
+	}
+
+	static async load(name) {
+		return JSON.parse(localStorage.getItem(name))
+	}
+}
