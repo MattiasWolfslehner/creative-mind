a7c905eb6991c20b0e9859508eb4f40be5243135
schnalzeneberger0502 <i.schnalzenberger@students.htl-leonding.ac.at>
2023-11-18 17:21:01 +0100

    fast render method for list

--- frontend/public/lib/components/idea-list.ts
+++ frontend/public/lib/components/idea-list.ts
@@ -2,7 +2,7 @@
 
 import {LitElement, html} from 'lit';
 import {customElement} from 'lit/decorators.js';
-import '../script/types'
+import '../script/types';
 import '../style/main.css';
 import '../style/style.scss';
 
@@ -26,41 +26,25 @@ export class IdeaList extends LitElement {
   }
 
   override render() {
-    console.log(`now render data`);
-
-    let table = document.createElement('table');
-    let thead =document.createElement("thead");
-    let row = document.createElement("tr");
-    let col = document.createElement("th");
-    col.innerHTML="Id";
-    row.appendChild(col);
-    col = document.createElement("th");
-    col.innerHTML="Content";
-    row.appendChild(col);
-    thead.appendChild(row);
-    table.appendChild(thead);
-    let tbody = document.createElement("tbody");
-
-    this.ideas.map((idea) => {
-      let row = document.createElement('tr');
-      let col = document.createElement('td');
-      col.innerHTML = idea.id.toString();
-      row.appendChild(col);
-      col = document.createElement('td');
-      col.innerHTML = idea.content.toString();
-      row.appendChild(col);
-      tbody.appendChild(row);
-    });
-    table.appendChild(tbody);
-    const page = html`
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        ${table}
+    return html`
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <table>
+        <thead>
+        <tr>
+          <th>Id</th>
+          <th>Content</th>
+        </tr>
+        </thead>
+        <tbody>
+        ${this.ideas.map(i => html`
+      <tr>
+        <td>${i.id.toString()}</td>
+        <td>${i.content.toString()}</td>
+      </tr>
+      `)}
+        </tbody>
+      </table>
     `;
-
-    return(page);
   }
 }
-
-
-customElements.define('idea-list', IdeaList);
