088ef7dc602d03dc341793356b3d34c2c77e49c0
MattiasWolfslehner <mattias.wolfslehner@gmail.com>
2023-11-20 18:11:43 +0100

    feat: get List of users (#53)

--- frontend/public/lib/script/api/api.ts
+++ frontend/public/lib/script/api/api.ts
@@ -1,1 +1,22 @@
 // TODO: put api logic here
+import axios from "axios";
+import { User } from "../types";
+
+async function getUsers(): Promise<User[]> {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }  
+    }
+
+    try {
+        const response = await axios.get('http://localhost:8080/api/users/list',config);
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    } 
+}
+
+
