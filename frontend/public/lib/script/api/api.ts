5511d0a6d6d55597689e1e4ecf9fd6780d6171c8
MattiasWolfslehner <mattias.wolfslehner@gmail.com>
2023-11-20 18:34:59 +0100

    feat: get Rooms (#53)

--- frontend/public/lib/script/api/api.ts
+++ frontend/public/lib/script/api/api.ts
@@ -1,8 +1,25 @@
 // TODO: put api logic here
 import axios from "axios";
-import { User } from "../types";
+import { Room, User } from "../types";
 
 const restPort = 8080;
+
+async function getRooms(): Promise<Room[]> {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    try {
+        const response = await axios.get(`http://localhost:${restPort}/api/rooms/list`,config);
+        console.log(response.data);
+        return response.data; 
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
 
 async function getUsers(): Promise<User[]> {
     const config = {
@@ -34,6 +51,6 @@ async function addUser() {
    });
 }
 
-
+export{getRooms};
 export{addUser};
 export{getUsers};
