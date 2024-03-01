9705889fd0adc6bbbc9699adff141f13f0e2887f
“Mattias <mattias.wolfslehner@gmail.com>
2024-03-01 09:48:31 +0100

    add keycloak to frontend and configure index.ts (#160)

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,4 +1,25 @@
+import Keycloak from 'keycloak-js';
+
+const keycloak = new Keycloak({
+    url: 'http://localhost:8000',
+    realm: 'cmr',
+    clientId: 'frontend'
+});
+
+async function init() {
+    try{
+        const authenticated = await keycloak.init({enableLogging:true});
+        console.log(`User is ${authenticated ? 'authenticated': 'not authenticated'}`);
+        if(!authenticated){
+            await keycloak.login();
+        }
+    }catch(error){
+        console.error('Failed to initialize adapter: ', error);
+        
+    }
+}
+init();
+
 import "./components/app"
 
-console.log("Hallo Welt!")
 
