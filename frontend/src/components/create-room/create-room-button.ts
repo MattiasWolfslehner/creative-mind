f16dfb80cc8e6776b210e3abd83fb47e72601e1e
“Mattias <mattias.wolfslehner@gmail.com>
2024-03-22 10:07:19 +0100

    adjust create-room-button

--- frontend/src/components/create-room/create-room-button.ts
+++ frontend/src/components/create-room/create-room-button.ts
@@ -1,14 +1,20 @@
 import { html, render } from "lit-html"
 
 const template = ()=> html`
-    <div style="margin-top: 30vh; display: flex; flex-wrap: wrap; justify-content: space-around;">
-        <div id="createRoomButton" style="background-color: white; width: 20vw; height: auto; text-align: center; font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px">
-            <h2>Create Room</h2>
-        </div>
-    </div>
+
 `
 
 class CreateRoomButtonElement extends HTMLElement {
+    template(){
+        return html`
+        <div style="margin-top: 30vh; display: flex; flex-wrap: wrap; justify-content: space-around; cursor:pointer">
+            <div id="createRoomButton" @click= ${() => this.createRoom()}
+            style="background-color: white; width: 20vw; height: auto; text-align: center; font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px">
+                <h2 style="user-select: none">Create Room</h2>
+            </div>
+        </div>
+        `
+    }
 
     constructor() {
         super()
@@ -17,8 +23,14 @@ class CreateRoomButtonElement extends HTMLElement {
 
     connectedCallback() {
         console.log("connected")
-        render(template(), this.shadowRoot)
+        render(this.template(), this.shadowRoot)
     }
+    
+    createRoom(){
+        console.log("create room pressed!");
+        
+    }
+
 }
 
 
