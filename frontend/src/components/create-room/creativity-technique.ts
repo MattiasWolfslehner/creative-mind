3c8e864e0c505907aac96ea296250f30e550380e
GitChrisHTL <c.ursprung@HTBLALeonding.onmicrosoft.com>
2024-04-24 12:43:13 +0200

    began working on task 171

--- frontend/src/components/create-room/creativity-technique.ts
+++ frontend/src/components/create-room/creativity-technique.ts
@@ -3,7 +3,7 @@ import "../../style/create-room/creativity-technique-style.css"
 
 const template = ()=> html`
     <div id="creativityTechniques" style="margin-top: 3vh; display: flex; flex-wrap: wrap; justify-content: space-around;">
-        <div id="sixThreeFive" style="background-color: #8D63D0; width: 20vw; height: auto; text-align: center; color: white; font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px">
+        <div id="sixThreeFive" style="background-color: rgba(141, 99, 208, 0.4); width: 20vw; height: auto; text-align: center; color: rgba(255, 255, 255, 0.4); font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px">
             <h2>6-3-5</h2>
         </div>
         <div id="mindMap" style="background-color: rgba(141, 99, 208, 0.4); width: 20vw; height: auto; text-align: center; color: rgba(255, 255, 255, 0.4); font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px">
@@ -11,6 +11,9 @@ const template = ()=> html`
         </div>
         <div id="zwickyBox" style="background-color: rgba(141, 99, 208, 0.4); width: 20vw; height: auto; text-align: center; color: rgba(255, 255, 255, 0.4); font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px">
             <h2>Zwicky-Box</h2>
+        </div>
+        <div id="brainstorm" style="background-color: #8D63D0; width: 20vw; height: auto; text-align: center; color: white; font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px">
+            <h2>Brainstorm</h2>
         </div>
     </div>
 `
