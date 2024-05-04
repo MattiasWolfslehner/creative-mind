f670b1f56c7f75953c7a0aab859379ede81c148f
schnalzenberger0502 <i.schnalzenberger@students.htl-leonding.ac.at>
2024-05-04 15:56:18 +0200

    add automatic detection of pod and automatic port forward

--- backend/k8s/start-db.sh
+++ backend/k8s/start-db.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-kubectl apply -f postgres.yaml || echo "Couldn't start DB, please ensure that minikube is running" >&2
+kubectl apply -f postgres.yaml || echo "Couldn't start DB, please ensure that minikube is running" >&2 && exit
+
+kubectl get pods ##(podname von postgres-xxxxx-yy)-- derzeit  postgres-6bf68995d9-phfrg
+kubectl port-forward $(kubectl get pods | grep --perl-regex -o '(post\S+)') 5432:5432 -n default
+
