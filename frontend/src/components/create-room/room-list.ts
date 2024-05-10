6ebe168ddbda6e169834af33134d11473ae651d6
schnalzenberger0502 <i.schnalzenberger@students.htl-leonding.ac.at>
2024-05-10 20:51:53 +0200

    adjust room list to be able to join room

---
+++ frontend/src/components/create-room/room-list.ts
@@ -0,0 +1,89 @@
+import { html, render } from "lit-html"
+import { store } from "../../model/store"
+import {Model, Room} from "src/model"
+import {produce} from "immer";
+import roomService from "../../service/room-service";
+import {router} from "../../../router";
+
+
+class RoomList extends HTMLElement {
+
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: "open" });
+    }
+
+    template(model: Model) {
+        const roomTemplates = model.rooms.map(room => html`
+        <tr>
+        <td>${room.roomId}</td>
+        <td>${room.roomState?"closed":"open"}</td>
+        <td>${room.type}</td>
+        <td>
+            <div id="${room.roomId}"  ?hidden = ${room.roomState} @click="${() => this._roomJoined(room.roomId)}"
+                 style="background-color: white; width: 20vw; height: auto; text-align: center; 
+                 font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px; cursor:pointer">
+            <h2 style="user-select: none">Join</h2>
+            </div>
+        </td>
+        </tr>
+        `);
+        return html`
+        <h1>Room List</h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Room</th>
+                <th>Type</th>
+                <th>Join</th>
+            </tr>
+            </thead>
+            <tbody>
+            ${roomTemplates}
+            </tbody>
+        </table>
+
+        <div id="showRoomButton"
+             style="background-color: white; width: 20vw; height: auto; text-align: center; 
+             font-family: 'sans-serif'; margin-bottom: 20px; border-radius: 10px; cursor:pointer">
+            <h2 style="user-select: none">Show Create Room</h2>
+        </div>
+        `;
+    }
+
+    connectedCallback() {
+        store.subscribe(model => {
+            //console.log(model);
+            render(this.template(model), this.shadowRoot)
+        });
+
+        this.addClickEventListeners();
+    }
+    private addClickEventListeners(): void {
+        const showRoomButton = this.shadowRoot.getElementById('showRoomButton');
+        showRoomButton.addEventListener('click', () => {
+            const model = produce(store.getValue(), draft => {
+                draft.isRoomList = false;
+                draft.activeRoomId = "";
+            });
+            store.next(model);
+        });
+    }
+    private async _roomJoined(_roomId: string) {
+        console.log(`Joining room with room id: ${_roomId}<`);
+        const roomId: Promise<void | Room> = roomService.getRoom(_roomId).then(value => {
+            const model = produce(store.getValue(), draft => {
+                draft.rooms.push(value);
+                draft.activeRoomId = value.roomId;
+            });
+            store.next(model);
+
+            router.navigate(`/room/${value.roomId}`);
+        });
+    }
+
+}
+
+customElements.define("room-list", RoomList)
