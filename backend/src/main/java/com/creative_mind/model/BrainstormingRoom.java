48c05a6c46a4db9287633e6a69623eae257e9a03
schnalzenberger0502 <i.schnalzenberger@students.htl-leonding.ac.at>
2024-05-10 20:43:20 +0200

    needed DiscriminatorValue

--- backend/src/main/java/com/creative_mind/model/BrainstormingRoom.java
+++ backend/src/main/java/com/creative_mind/model/BrainstormingRoom.java
@@ -1,8 +1,10 @@
 package com.creative_mind.model;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Brainstorming Room")
 public class BrainstormingRoom extends IdeaRoom{
     private static final String ROOM_TYPE = String.valueOf(RoomType.BRAINSTORMING);
 
