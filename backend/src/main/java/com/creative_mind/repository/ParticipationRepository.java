3b2a6c0c2ed14d502b88b56cc9efcc63f7790053
schnalzenberger0502 <i.schnalzenberger@students.htl-leonding.ac.at>
2024-05-11 16:14:33 +0200

    delete participation was wrong (used roomId and userId instead of room.id and user.id) REJOIN to room was impossible! BUGFIX

--- backend/src/main/java/com/creative_mind/repository/ParticipationRepository.java
+++ backend/src/main/java/com/creative_mind/repository/ParticipationRepository.java
@@ -28,6 +28,15 @@ public class ParticipationRepository {
     public List<Participation> getAllParticipation() {
         return this.entityManager.createNamedQuery(Participation.QUERY_FIND_ALL, Participation.class).getResultList();
     }
+    public Participation getParticipation(Integer userId, Integer roomId ) {
+        TypedQuery<Participation> query = entityManager.createNamedQuery(Participation.QUERY_FIND_ONE, Participation.class);
+        Participation participation = query
+                .setParameter("userId", userId)
+                .setParameter("roomId", roomId)
+                .getSingleResult();
+
+        return participation;
+    }
 
     @Transactional
     public void addParticipation(ParticipantionRequest participationRequest) {
@@ -54,10 +63,12 @@ public class ParticipationRepository {
         User member = userRepository.getUserByUUID(participationRequest.getMemberId());
         Room room = roomRepository.getRoomByUUID(participationRequest.getRoomId());
 
-        Participation participation = new Participation(room, member, sessionId);
+        Participation participation = getParticipation(room.getId(), member.getId());
 
-        if (this.isUserInRoom(member.getId(), room.getId())) {
+        if (participation != null) {
+            //Participation p = this.entityManager.merge(participation); // recommended with persistance
             this.entityManager.remove(participation);
+            Log.info(String.format("delete participation User[%s] in Room[%s] ([%s] <> [%s])!", member.getId(), room.getId(), participation.getSessionId(), sessionId));
         } else {
             throw new CreativeMindException(String.format("User[%s] is not in Room[%s]!", member.getId(), room.getId()));
         }
